3410389af2dc5284b4e00715ce861f0228cf20e6
Suresh Gv <gvsuresh738@gmail.com>
2023-04-24 12:31:13 +0530

    webpack configured successfully

---
+++ css.d.ts
@@ -0,0 +1,24 @@
+declare module '*.scss' {
+    const css: { [key: string]: string };
+    export default css;
+}
+declare module '*.sass' {
+    const css: { [key: string]: string };
+    export default css;
+}
+declare module 'react-markup';
+declare module '*.webp';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+
+declare module '*.svg?inline' {
+    const content: any
+    export default content
+}
+
+declare module '*.svg' {
+    const content: any
+    export default content
+}
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+    return(
+        <div data-testid='app'>
+            <h1>Happy Hacking</h1>
+        </div>
+    )
+}
+
+export default App;
